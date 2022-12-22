a60895c96ee9e59a46561fb0fe30aeb5b3d2ac39
saulo.sa <srssaulo@gmail.com>
2022-12-22 17:33:07 -0300

    en string def CertficateSendTask

--- lang/en/local_pnp.php
+++ lang/en/local_pnp.php
@@ -32,5 +32,7 @@ $string['uribase'] = 'External base URI';
 $string['uribase_'] = 'External base URI';
 $string['token'] = 'Token';
 $string['token_'] = 'Token to connect external service';
+$string['certficatesendtask'] = 'Sync certificate task PNP';
+
 
 
