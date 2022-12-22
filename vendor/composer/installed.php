c4a14225ea1623ee8951a1089df41ad1e21560b4
saulo.sa <srssaulo@gmail.com>
2022-12-22 17:01:52 -0300

    with vendor folder and .gitignore altered

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9fddf207fc2efb9d554244b118ee35aaaa2053d0',
+        'name' => 'srssaulo/pnp',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sendgrid/php-http-client' => array(
+            'pretty_version' => '4.0.0',
+            'version' => '4.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sendgrid/php-http-client',
+            'aliases' => array(),
+            'reference' => 'fbfc0471541b42101da932a0dbb14b0fdf3d0000',
+            'dev_requirement' => false,
+        ),
+        'srssaulo/pnp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9fddf207fc2efb9d554244b118ee35aaaa2053d0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
