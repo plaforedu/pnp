48590ad7887394df52aae7ad1f1558bdbb61031d
saulo.sa <srssaulo@gmail.com>
2023-01-18 20:19:17 -0300

    compiled vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '20eeef47ba55208dfdab6c4cea32381e6efcd1c9',
+        'name' => 'srssaulo/pnp',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sendgrid/php-http-client' => array(
+            'pretty_version' => '4.0.0',
+            'version' => '4.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sendgrid/php-http-client',
+            'aliases' => array(),
+            'reference' => 'fbfc0471541b42101da932a0dbb14b0fdf3d0000',
+            'dev_requirement' => false,
+        ),
+        'srssaulo/pnp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '20eeef47ba55208dfdab6c4cea32381e6efcd1c9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
