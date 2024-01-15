1a5c7e603cd2756bc96f35e2f51283348458ef7a
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-01-15 17:28:34 -0300

    feat: [add] tradução en, pt_br

--- lang/en/local_pnp.php
+++ lang/en/local_pnp.php
@@ -20,19 +20,19 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Send Certificate Issues PNP' ;
-$string['privacy:metadata'] = 'This block only offers the possibility to apply standard operations on multiple course modules at the same time.
+$string['pluginname'] = 'Get/Send Certificate Issues PNP';
+$string['privacy:metadata'] = 'This local plugin only offers the possibility to apply standard operations on multiple course modules at the same time.
 Thus, no data is being stored by this block.';
-$string['pnpconfs'] = 'Send Certficate Issued PNP confs';
+$string['pnpconfs'] = 'Get/Send Certficate Issued PNP confs';
 
 //SETTINGS FIELDS
-$string['certid'] = 'Certificate ID';
-$string['certid_'] = 'Moodle Certificate ID to check issued';
+$string['certid'] = 'Certificate IDs';
+$string['certid_'] = 'Moodle Certificate IDs to check issued. Separate using comma.';
 $string['uribase'] = 'External base URI';
 $string['uribase_'] = 'External base URI';
 $string['token'] = 'Token';
 $string['token_'] = 'Token to connect external service';
 $string['certificatesendtask'] = 'Sync certificate task PNP';
 
-
-
+$string['auth_token'] = 'Authentication token to get certificates of specified CPF';
+$string['auth_token_'] = 'Authentication token to external service get certificates of specified CPF';
---
+++ lang/pt_br/local_pnp.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @author     Saulo Sá <srssaulo@gmail.com>
+ * @package    block_pnp
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Lê/envia certificados da PNP';
+$string['privacy:metadata'] = 'Este plugin local oferece apenas a possibilidade de aplicar operações padrão em vários módulos de curso ao mesmo tempo. Portanto, nenhum dado é armazenado por este bloco1.';
+$string['pnpconfs'] = 'Configurações para ler/enviar certificados da PNP';
+
+//SETTINGS FIELDS
+$string['certid'] = 'IDs dos certificados';
+$string['certid_'] = 'IDs de certificados Moodle emitidos. Separe usando vírgula.';
+$string['uribase'] = 'URI externo para envio';
+$string['uribase_'] = 'URI externo para envio';
+$string['token'] = 'Token externo para envio';
+$string['token_'] = 'Token externo para envio';
+$string['certificatesendtask'] = 'Tarefa de sincronização de certificado PNP';
+
+$string['auth_token'] = 'Token de autenticação para obter certificados de CPF específicos';
+$string['auth_token_'] = 'Token de autenticação para obter certificados de CPF específicos de um serviço externo';
